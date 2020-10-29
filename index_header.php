e6ff67e20dbe5f29d5d9a12a6657724a1e79c802
daeclerck <59842527+daeclerck@users.noreply.github.com>
2020-10-29 16:27:08 -0500

    Add files via upload

---
+++ index_header.php
@@ -0,0 +1,14 @@
+<!--NavBar-->
+<div>
+  <a href="FILE HERE">Catalog Page</a>
+  <?php echo " || "; ?>
+  <a href="FILE HERE">Orders/a>
+  <?php echo " || "; ?>
+  <a href="FILE HERE">Reception Desk</a>
+  <?php echo " || "; ?>
+  <a href="FILE HERE">Warehouse</a>
+  <?php echo " || "; ?>
+  <a href="FILE HERE">Invoice</a>
+  <?php echo " || "; ?>
+  <a href="FILE HERE">Admin</a>
+</div>
